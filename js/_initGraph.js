e041615f6fe695878b54cc2e6c93bebda09d0283
Ludovic Anterieur <ludovic.anterieur@serviceonnewgrounds.com>
2022-11-16 11:35:58 +0800

    pan & scale

--- js/_initGraph.js
+++ js/_initGraph.js
@@ -1,6 +1,7 @@
 import { setShapes } from "/js/_shapes.js";
 
 export function initGraph(graphEl) {
+  var dragStartPosition = { x: 0, y: 0 };
   var namespace = joint.shapes;
 
   var graph = new joint.dia.Graph({}, { cellNamespace: namespace });
@@ -14,32 +15,34 @@ export function initGraph(graphEl) {
     cellViewNamespace: namespace,
     background: { color: '#F3F7F6' },
     interactive: { linkMove: false },
-    
+    dragStartPosition,
     defaultConnectionPoint: {
-        name: 'boundary'
+      name: 'boundary'
     },
     defaultAnchor: {
-        name: 'center'
+      name: 'center'
     },
     sorting: joint.dia.Paper.sorting.APPROX,
-    viewport: function(view) {
-        var element = view.model;
-        // Hide any element or link which is embedded inside a collapsed parent (or parent of the parent).
-        var hidden = element.getAncestors().some(function(ancestor) {
-            return ancestor.isCollapsed();
-        });
-        return !hidden;
+    viewport: function (view) {
+      var element = view.model;
+      // Hide any element or link which is embedded inside a collapsed parent (or parent of the parent).
+      var hidden = element.getAncestors().some(function (ancestor) {
+        return ancestor.isCollapsed();
+      });
+      return !hidden;
     }
   });
   paper.el.style.border = '1px solid #E2E2E2';
-
+  let scale = {sx:1,sy:1};
   function zoomOnMousewheel(paper, delta) {
     if (_.isNull(paper) || _.isUndefined(paper)) return;
 
-    const scale = paper.scale();
+    scale = paper.scale();
     const newScaleX = scale.sx + (delta * 0.01);
     const newScaleY = scale.sy + (delta * 0.01);
     if (newScaleX >= 0.2 && newScaleX <= 2) paper.scale(newScaleX, newScaleY);
+
+    // dragStartPosition = { x: x * scale.sx, y: y * scale.sy };
   }
   paper.on({
     'blank:mousewheel': (event, x, y, delta) => {
@@ -49,7 +52,21 @@ export function initGraph(graphEl) {
     'cell:mousewheel': (_, event, x, y, delta) => {
       event.preventDefault();
       zoomOnMousewheel(paper, delta);
+    },
+    'blank:pointerdown': function (event, x, y) {
+      dragStartPosition.x = x * scale.sx;
+      dragStartPosition.y = y * scale.sy;
+    },
+    'cell:pointerup blank:pointerup': function (cellView, x, y) {
+      delete dragStartPosition.x; delete dragStartPosition.y;
     }
+  });
+
+  $(graphEl).mousemove(function (event) {
+    if (dragStartPosition.x)
+      paper.translate(
+        event.offsetX - dragStartPosition.x,
+        event.offsetY - dragStartPosition.y);
   });
 
   setShapes(joint);
